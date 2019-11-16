fb21551af4ddef13c3022da233c879d6380d7ff7
saanayy <sanay.nitjsr@gmail.com>
2019-11-16 21:07:50 +0530

    utilities added

--- app/src/main/java/com/bytebucket/medico/utilities/Constants.java
+++ app/src/main/java/com/bytebucket/medico/utilities/Constants.java
@@ -1,5 +1,39 @@
 package com.bytebucket.medico.utilities;
 
+import com.bytebucket.medico.R;
+
 public class Constants {
     final public static int WORDS_PER_MINUTE = 150;
+    final public static int[] PROFILE_IMAGES = {
+            R.drawable.ic_doctor_one,
+            R.drawable.ic_doctor_two,
+            R.drawable.ic_doctor_three,
+            R.drawable.ic_doctor_four,
+            R.drawable.ic_doctor_five,
+    };
+
+    final public static int[] ARTICLE_IMAGES = {
+            R.drawable.ic_article_image_one,
+            R.drawable.ic_article_image_two,
+            R.drawable.ic_article_image_three,
+            R.drawable.ic_article_image_four,
+            R.drawable.ic_article_image_five,
+            R.drawable.ic_article_image_six,
+            R.drawable.ic_article_image_seven,
+            R.drawable.ic_article_image_eight
+    };
+
+
+    public static int getRandomProfilePic(){
+        int len = PROFILE_IMAGES.length;
+        int randomInt = (int)(Math.random() * ((len - 0)));
+        return PROFILE_IMAGES[randomInt];
+    }
+
+
+    public static int getRandomArticlePic(){
+        int len = ARTICLE_IMAGES.length;
+        int randomInt = (int)(Math.random() * ((len - 0)));
+        return ARTICLE_IMAGES[randomInt];
+    }
 }
